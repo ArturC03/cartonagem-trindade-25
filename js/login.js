696c16824dc9bd021df930664de382e1413e8a9a
hugo2006alm <hugo2006almeida2006@gmail.com>
2023-11-24 18:06:38 +0000

    New Login Page

---
+++ js/login.js
@@ -0,0 +1,32 @@
+
+
+function recuperarPass() {
+    const login = document.getElementById("login");
+    const recuperarPass = document.getElementById("recuperarPass");
+    const flipboxinner = document.getElementById("flipboxinner");
+
+    if (login.classList.contains("active")) {
+        login.classList.remove("active");
+        recuperarPass.classList.add("active");
+
+        flipboxinner.style.transform = "rotateY(180deg)";
+    } else {
+        recuperarPass.classList.remove("active");
+        login.classList.add("active");
+
+        flipboxinner.style.transform = "rotateY(0deg)"; 
+    }
+}
+
+
+function voltarLogin() {
+    const login = document.getElementById("login");
+    const recuperarPass = document.getElementById("recuperarPass");
+    const flipboxinner = document.getElementById("flipboxinner");
+
+    recuperarPass.classList.remove("active");
+    login.classList.add("active");
+
+    flipboxinner.style.transform = "rotateY(0deg)"; 
+}
+
