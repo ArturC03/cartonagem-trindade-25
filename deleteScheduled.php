3fd3c0bb8211cab6cb725770c76418f33441c626
Hugo Almeida <hugo2006almeida2006@gmail.com>
2024-01-26 16:15:04 +0000

    Fixed Deletion of Non-Existing Task

--- deleteScheduled.php
+++ deleteScheduled.php
@@ -7,17 +7,23 @@ $agendamento = my_query("SELECT * FROM hora WHERE id_hora = " . $id . ";");
 $result = my_query("DELETE FROM hora WHERE id_hora = " . $id . ";");
 
 if ($result) {
-    if (exec('schtasks /delete /tn "Exportação Agendada ' . $id . '" /f 2>&1', $output)) {
+    if ($agendamento[0]['periodo_geracao'] != "ONCE" || date("Y-m-d", $agendamento[0]['data_geracao']) >= date("Y-m-d")) {
+        if (exec('schtasks /delete /tn "Exportação Agendada ' . $id . '" /f 2>&1', $output)) {
+            array_map('unlink', glob(__DIR__ . "/download/scheduled/" . $id . "/*.*"));
+            rmdir(__DIR__ . "/download/scheduled/" . $id);
+    
+            header('Location: csvtimes.php');
+        } else {
+            my_query("INSERT INTO hora (id_hora, periodo_geracao, data_geracao, sensores, tipo_geracao) VALUES (" . $agendamento[0]['id_hora'] . " ,' " . $agendamento[0]['periodo_geracao'] . "', '" . $agendamento[0]['data_geracao'] . "', '" . $agendamento[0]['sensores'] . "', " . $agendamento[0]['tipo_geracao'] . ");");
+            echo $output;
+            echo "Não foi possível eliminar a tarefa. Tente outra vez.";
+        }
+    } else {
         array_map('unlink', glob(__DIR__ . "/download/scheduled/" . $id . "/*.*"));
         rmdir(__DIR__ . "/download/scheduled/" . $id);
 
         header('Location: csvtimes.php');
-    } else {
-        my_query("INSERT INTO hora (id_hora, periodo_geracao, data_geracao, sensores, tipo_geracao) VALUES (" . $agendamento[0]['id_hora'] . " ,' " . $agendamento[0]['periodo_geracao'] . "', '" . $agendamento[0]['data_geracao'] . "', '" . $agendamento[0]['sensores'] . "', " . $agendamento[0]['tipo_geracao'] . ");");
-        echo $output;
-        echo "Não foi possível eliminar a tarefa. Tente outra vez.";
     }
-    
 } else {
     echo "Não foi possível eliminar o registo. Tente outra vez.";
 }
