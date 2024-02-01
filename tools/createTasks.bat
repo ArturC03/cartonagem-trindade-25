4c2b41a155b3f8899cb90f4012a23dd0f50c01ea
Hugo Almeida <hugo2006almeida2006@gmail.com>
2024-02-01 17:33:18 +0000

    Changed Hourly to run at hh:01

--- tools/createTasks.bat
+++ tools/createTasks.bat
@@ -3,11 +3,14 @@
 REM Get the path to the PHP executable
 for /f "delims=" %%i in ('where php') do set PHP_PATH=%%i
 
+REM Get the current hour
+for /f "tokens=1 delims=:" %%a in ("%time%") do set CURRENT_HOUR=%%a
+
 REM Create MINUTE task
 schtasks /Create /SC MINUTE /TN "Agendamentos Minuto a Minuto" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" MINUTE"
 
 REM Create HOURLY task
-schtasks /Create /SC HOURLY /TN "Agendamentos Horários" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" HOURLY"
+schtasks /Create /SC HOURLY /TN "Agendamentos Horários" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" HOURLY" /ST %CURRENT_HOUR%:01
 
 REM Create DAILY task
 schtasks /Create /SC DAILY /TN "Agendamentos Diários" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" DAILY" /ST 00:01
