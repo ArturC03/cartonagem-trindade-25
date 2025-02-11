9bb8766f3a94d9f4c7f6fce003660fef4693564e
ArturC03 <arturvicentecruz@proton.me>
2025-02-11 16:02:14 +0000

    check

--- DB/create_new_db.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7d3614b52fbde4936d971f7c2059d261038f2fc7855d30bfbc038674fe83f1aa
-size 3550
--- DB/migration.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6f8a97bee03d2b069adf45f6c49b0cfd4e270890c50405052756bf060c604fa0
-size 5887
